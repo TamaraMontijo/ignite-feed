35f99ba85c392c4f4deefba1cd1310c00c7b1a89
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2024-01-24 18:02:42 -0300

    feat: post

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,77 @@
-import { Post } from './components/Post';
-import { Header } from './components/Header';
-import { Sidebar } from './components/Sidebar';
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
-import './global.css';
+import "./global.css";
 
+// author: {avatar_url: "", name "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:
+        "https://media.licdn.com/dms/image/D4D03AQEn7JPvjHWoIA/profile-displayphoto-shrink_400_400/0/1704826509874?e=1711584000&v=beta&t=AmQ9uf5_k0fVLxG1GRKU7tf2Mb61onpY4wQu5fBGC0I",
+      name: "Tamara Montijo",
+      role: "Frontend Engineer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É umprojeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-01-23 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É umprojeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-01-20 20:00:00"),
+  },
+];
 
 function App() {
-
   return (
     <div>
       <Header />
-      
+
       <div className={styles.wrapper}>
-      <Sidebar />
-      <main>
-      <Post 
-      author="'Tamara Montijo'"
-      content="'Lorem ipsum'"
-      />
-      <Post 
-      author="Teste"
-      content="Oi"
-      />
-      </main>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
       </div>
-      
-      
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
